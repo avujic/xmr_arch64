7f360f35d11b18dc545f273100b269bdfba01570
avujic <avujic@gmail.com>
2018-04-09 21:20:41 +0200

    XMR new PoW adjustments #13

--- src/Mem_unix.cpp
+++ src/Mem_unix.cpp
@@ -22,77 +22,58 @@
  */
 
 
-#include <stdlib.h>
-#include <sys/mman.h>
-
-
-#if defined(XMRIG_ARM) && !defined(__clang__)
-#   include "aligned_malloc.h"
-#else
-#   include <mm_malloc.h>
+#ifdef __FreeBSD__
+#   include <sys/types.h>
+#   include <sys/param.h>
+#   include <sys/cpuset.h>
+#   include <pthread_np.h>
 #endif
 
 
-#include "crypto/CryptoNight.h"
-#include "log/Log.h"
-#include "Mem.h"
-#include "Options.h"
+#include <pthread.h>
+#include <sched.h>
+#include <unistd.h>
+#include <string.h>
 
 
-bool Mem::allocate(int algo, int threads, bool doubleHash, bool enabled)
+#include "Cpu.h"
+
+
+#ifdef __FreeBSD__
+typedef cpuset_t cpu_set_t;
+#endif
+
+
+void Cpu::init()
 {
-    m_algo       = algo;
-    m_threads    = threads;
-    m_doubleHash = doubleHash;
-
-    const int ratio   = (doubleHash && algo != Options::ALGO_CRYPTONIGHT_LITE) ? 2 : 1;
-    const size_t size = MEMORY * (threads * ratio + 1);
-
-    if (!enabled) {
-        m_memory = static_cast<uint8_t*>(_mm_malloc(size, 16));
-        return true;
-    }
-
-    m_flags |= HugepagesAvailable;
-
-#   if defined(__APPLE__)
-    m_memory = static_cast<uint8_t*>(mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, VM_FLAGS_SUPERPAGE_SIZE_2MB, 0));
-#   elif defined(__FreeBSD__)
-    m_memory = static_cast<uint8_t*>(mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_ALIGNED_SUPER | MAP_PREFAULT_READ, -1, 0));
-#   else
-    m_memory = static_cast<uint8_t*>(mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_POPULATE, 0, 0));
+#   ifdef XMRIG_NO_LIBCPUID
+    m_totalThreads = sysconf(_SC_NPROCESSORS_CONF);
 #   endif
-    if (m_memory == MAP_FAILED) {
-        m_memory = static_cast<uint8_t*>(_mm_malloc(size, 16));
-        return true;
-    }
 
-    m_flags |= HugepagesEnabled;
-
-    if (madvise(m_memory, size, MADV_RANDOM | MADV_WILLNEED) != 0) {
-        LOG_ERR("madvise failed");
-    }
-
-    if (mlock(m_memory, size) == 0) {
-        m_flags |= Lock;
-    }
-
-    return true;
+    initCommon();
 }
 
 
-void Mem::release()
+void Cpu::setAffinity(int id, uint64_t mask)
 {
-    const int size = MEMORY * (m_threads + 1);
+    cpu_set_t set;
+    CPU_ZERO(&set);
 
-    if (m_flags & HugepagesEnabled) {
-        if (m_flags & Lock) {
-            munlock(m_memory, size);
+    for (int i = 0; i < m_totalThreads; i++) {
+        if (mask & (1UL << i)) {
+            CPU_SET(i, &set);
         }
-
-        munmap(m_memory, size);
     }
-    else {
-        _mm_free(m_memory);
+
+    if (id == -1) {
+#       ifndef __FreeBSD__
+        sched_setaffinity(0, sizeof(&set), &set);
+#       endif
+    } else {
+#       ifndef __ANDROID__
+        pthread_setaffinity_np(pthread_self(), sizeof(&set), &set);
+#       else
+        sched_setaffinity(gettid(), sizeof(&set), &set);
+#       endif
     }
 }
